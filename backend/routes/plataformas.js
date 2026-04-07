5146573a44d6619543af6d49c55725db44683c2b
lmbnkk <limbonikk@gmail.com>
2026-04-07 20:10:00 +0200

    CRUD de plataformas y añadido .gitignore

--- backend/routes/plataformas.js
+++ backend/routes/plataformas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../database'); // Importamos la conexión de database.js
 const { body, validationResult } = require('express-validator'); // libreria para validaciones
 
-// GET --> Obtener todas las plataformas
+// READ - GET --> Obtener todas las plataformas
 router.get('/', (req, res) => {
     db.all('SELECT * FROM plataformas', [], (err, rows) => {
         if (err) {
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     });
 });
 
-// POST --> Crear una nueva plataforma
+// CREATE - POST --> Crear una nueva plataforma
 router.post('/', 
     // Validación
     body('nombre').isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
@@ -35,5 +35,41 @@ router.post('/',
         });
     }
 );
+
+// UPDATE - PUT --> Actualizar una plataforma existente
+router.put('/:id',
+    // Validación
+    body('nombre').isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres'),
+    (req, res) => {
+        // Comprobar si hay errores de validación 
+        const errores = validationResult(req);
+        if (!errores.isEmpty()) {
+            return res.status(400).json({ errores: errores.array() });
+        }
+
+        const { id } = req.params;
+        const { nombre, fabricante } = req.body;
+        db.run("UPDATE plataformas SET nombre = ?, fabricante = ? WHERE id =?", [nombre, fabricante, id], function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            if (this.changes === 0) {
+                return res.status(404).json({ error: 'Plataforma no encontrada' });
+            }
+            res.json({ id, nombre, fabricante });
+        });
+    }
+);
+
+// DELETE  --> Eliminar una plataforma existente
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db.run("DELETE FROM plataformas WHERE id = ?", [id], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json({ message: 'Plataforma eliminada correctamente' });
+    });
+});
 
 module.exports = router;
