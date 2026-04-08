6e64eb1ea50d394bd33f0f769e476eef73c67793
lmbnkk <limbonikk@gmail.com>
2026-04-08 19:29:06 +0200

    FIX: Añadido el activar las FK

--- backend/database.js
+++ backend/database.js
@@ -9,6 +9,16 @@ const db = new sqlite3.Database(dbPath, (err) => {
         console.error('Error al abrir la base de datos:', err.message);
     } else {
         console.log('Conectado a SQLite.');
+
+        // Activar las Foreign Keys
+        db.run("PRAGMA foreign_keys = ON;", (pragmaErr) => {
+            if (pragmaErr) {
+                console.error('Error al activar Foreign Keys:', pragmaErr.message);
+            } else {
+                console.log('Integridad referencial (Foreign Keys) activada.');
+            }
+        });
+
     }
 });
 
